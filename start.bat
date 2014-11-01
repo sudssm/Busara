debc0944ed676dfea865bf05d742356d242c8d0f
Sudarshan Muralidhar <sudarshan.muralidhar@gmail.com>
2014-11-01 04:35:50 -0400

    fix httpd.conf bug

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
+del httpd.conf
+type httpd.conf-start >> httpd.conf
+echo DocumentRoot "%CD%" >> httpd.conf
+echo ^<Directory "%CD%"^> >> httpd.conf
+type httpd.conf-end >> httpd.conf
+pause
+
 copy httpd.conf C:\xampp\apache\conf\httpd.conf
 C:/xampp/xampp_start.exe
 
@@ -5,3 +12,4 @@ C:/xampp/xampp_start.exe
 mysql -u root < setup.sql
 IF %ERRORLEVEL%==1 GOTO IMPORT
 start http://localhost
+pause
